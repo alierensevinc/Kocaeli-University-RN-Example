416d0194a2aa6965d787944b1c46a7ef3a77235b
Ali Eren Sevinç <sevinc.aeren@gmail.com>
2024-11-04 21:15:56 +0300

    Build Basic UI

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+// App bileşeni, uygulamanın ana bileşenidir.
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        // Ana kapsayıcı View bileşeni
+        <View style={styles.container}>
+            {/* Başlık metni */}
+            <Text style={styles.title}>TO DO Uygulaması</Text>
+            {/* Girdi alanı ve butonun bulunduğu kapsayıcı View bileşeni */}
+            <View style={styles.inputContainer}>
+                {/* Görev adı için TextInput bileşeni */}
+                <TextInput style={styles.input} placeholder={'Görev Adı'} />
+                {/* Ekle butonu */}
+                <Button title="Ekle" />
+            </View>
+        </View>
+    );
 }
 
+// Stil tanımlamaları
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1, // Esnek yerleşim
+        backgroundColor: '#fff', // Arka plan rengi beyaz
+        alignItems: 'center', // Yatayda ortalama
+        justifyContent: 'flex-start', // Dikeyde başa hizalama
+        marginTop: 36 // Üstten boşluk
+    },
+    title: {
+        fontSize: 20, // Yazı boyutu
+        fontWeight: 'bold', // Kalın yazı
+    },
+    inputContainer: {
+        flexDirection: 'row', // Yatayda yerleşim
+        alignItems: 'center', // Yatayda ortalama
+        justifyContent: 'space-between', // Elemanlar arasında boşluk bırakma
+    },
+    input: {
+        height: 40, // Yükseklik
+        width: 200, // Genişlik
+        margin: 12, // Dış boşluk
+        borderWidth: 1, // Kenar kalınlığı
+        padding: 10, // İç boşluk
+    }
 });
